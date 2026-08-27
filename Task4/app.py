1fa9f19eb57cb1c935b0a766c3b1a55d800bc741
ehtishamjavaid <ahtashamjavaid9@gmail.com>
2026-08-28 00:52:47 +0500

    Complete Task 4 Kubernetes rolling update

--- Task4/app.py
+++ Task4/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Rhombix Technologies - CI/CD Pipeline is Working!"
+    return  "Rhombix Technologies - CI/CD Pipeline v2 is Working!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
